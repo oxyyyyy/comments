984f1ca6bdab87d43698ad4080f2bdb63040ec0f
Yone-e <yone.work@gmail.com>
2017-01-26 17:54:21 +0200

    added answer-system

--- js/controllers/comments.js
+++ js/controllers/comments.js
@@ -2,9 +2,9 @@
 
     angular
         .module("Comments")
-        .controller("commentCtrl", ['$scope', '$http', 'Backand', 'AuthService', CommentController]);
+        .controller("commentsCtrl", ['$scope', '$http', 'Backand', 'AuthService', CommentsController]);
 
-    function CommentController($scope, $http, Backand, AuthService){
+    function CommentsController($scope, $http, Backand, AuthService){
         var vm = this;
 
         var baseUrl = Backand.getApiUrl() + '/1/objects/';
@@ -12,42 +12,75 @@
 
         vm.currentUser = AuthService.currentUser;
 
+        vm.error = "";
+
+        //---
+        //postcomment-data
         vm.anonymous = false;
         vm.author = vm.currentUser.name || "";
         vm.text = "";
-
-        vm.error = "";
+        vm.answerTo = {
+                          id:"",
+                          name:""
+                      }; //accessable from commentslist
 
         vm.popoverOnAnon = {
             isOpen: true,
             templateUrl: 'myPopoverTemplate.html',
             close : function () {
                 vm.popoverOnAnon.isOpen = false;
-                console.log(22);
             },
             auth : function () {
                 vm.popoverOnAnon.close();
                 vm.showAuth()
             }
         };
+        //---
 
+        //---
+        //postcomment-methods
         vm.clearError = clearError;
 
         vm.setAnonymousPost = setAnonymousPost;
 
-        vm.initComment = initComment;
+        vm.initPostComment = initPostComment;
         vm.addComment = addComment;
 
         vm.showAuth = AuthService.showAuth;
         vm.logout = AuthService.logout;
+        //---
 
-        function initComment() {
+        //---
+        //commentlist-data
+        vm.data = [];
+        vm.commentsOnPage = 4;
+        vm.commentsDisplayIndex = 0;
+        //---
+
+        //---
+        //commentlist-methods
+        vm.removeComment = deleteComment;
+        vm.readComments = readCommentsList;
+        vm.commentOnCreation = initComment;
+        vm.allowUpdate = editComment;
+        vm.saveUpdatedComment = updateComment;
+
+        vm.setAnswerTo = setAnswerTo;
+
+        vm.validateDate = validateDate;
+
+        vm.setRows = textareaSetSize;
+
+        vm.nextComments = nextComments;
+        //---
+
+        //---
+        //postcomment-funcs
+        function initPostComment() {
             vm.text = "";
             vm.author = "";
-        }
-
-        function clearError() {
-            vm.error = null;
+            vm.answerTo.name = "";
+            vm.answerTo.id = "";
         }
 
         function setAnonymousPost() {
@@ -56,7 +89,7 @@
         function addComment() {
             if(vm.currentUser.name != "" && vm.text != ""){
                 sendToServer();
-                vm.initComment();
+                vm.initPostComment();
                 vm.anonymous = false;
             } else {
                 errHandler("Пустой комментарий");
@@ -70,12 +103,14 @@
                 data: {
                     author: vm.currentUser.name || vm.author,
                     comment: vm.text,
-                    anonymous: vm.anonymous
+                    anonymous: vm.anonymous,
+                    in_answer_to: vm.answerTo.id
                 },
                 params: {
                     returnObject: true
                 }
             }).then(function(response) {
+                console.log(response.data);
                 return response.data;
             }, errHandler);
         };
@@ -87,5 +122,108 @@
                 vm.error = null;
             },2000);
         };
+        //---
+
+        //---
+        //commentlist-funcs
+        Backand.on('comments_updated', function (data) {
+            vm.readComments();
+        });
+
+        function errHandler(err) {
+            console.error(err);
+            vm.error = err.data;
+            setTimeout(function() {
+                vm.error = null;
+            },2000);
+        }
+
+        function readCommentsList() {
+            return $http({
+                method: 'GET',
+                url: baseUrl + objectName
+            }).then(function(response) {
+                return response.data.data;
+            }).then(function(data){
+                vm.data = data;
+            });
+        };
+
+        function initComment() {
+            vm.error = null;
+            vm.data.editable = false;
+        };
+
+        function editComment(singleComment){
+            singleComment.editable = true;
+        };
+
+        function updateComment(singleComment) {
+            singleComment.editable = false;
+            return $http({
+                method: 'PUT',
+                data: {
+                    author: singleComment.author,
+                    comment: singleComment.comment
+                },
+                url: baseUrl + objectName + '/' + singleComment.id
+            }).then(function (response) {
+                return response.data;
+            }, errHandler);
+        }
+
+        function deleteComment(singleComment) {
+            return $http({
+                method: 'DELETE',
+                url : baseUrl + objectName + '/' + singleComment.id
+            }).then(function(response) {
+                return response.data;
+            }, errHandler);
+        };
+
+        function nextComments(direction) {
+            if(direction>0) {
+                if((vm.data.length / vm.commentsOnPage) > vm.commentsDisplayIndex+1)
+                    vm.commentsDisplayIndex++;
+            }
+            else {
+                if (vm.commentsDisplayIndex > 0)
+                    vm.commentsDisplayIndex--;
+            }
+        }
+
+        function validateDate(date) {
+            var newDate = new Date(date.toString());
+            //newDate.setTime (newDate.getTime() - newDate.getTimezoneOffset()*60*1000 );
+            return newDate.toString().slice(0,21);
+        }
+
+        function textareaSetSize(singleComment) {
+            var comments = $('.commentPostedText');
+            console.log(comments);
+            for(var i = 0; i < comments.length; i++){
+                var pos = 0;
+                var rows = 1;
+                while (true) {
+                    var foundpos = (singleComment.comment).indexOf('\n', pos);
+                    if (foundpos == -1) break;
+                    rows += 1;
+                    pos = foundpos + 1;
+                }
+                comments[i].rows = rows;
+                console.log(rows);
+            }
+        }
+
+        function setAnswerTo(singleComment) {
+            vm.answerTo.name = singleComment.author;
+            vm.answerTo.id = singleComment.id;
+        }
+        //---
+
+        function clearError() {
+            vm.error = null;
+        }
+
     }
 })();
